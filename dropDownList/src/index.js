6eb26b4a588747ecd089dd6e1ab8dca9cf4aad26
Cody Lindley <contact@codylindley.com>
2016-07-25 12:11:29 -0600

    continue to simplify

--- dropDownList/src/index.js
+++ dropDownList/src/index.js
@@ -11,12 +11,13 @@ const KendoDropDownList = React.createClass({
 	//component is in the DOM, so do stuff to it in this callback
 	componentDidMount: function() {
 		//get, child element node for this component
-		var elementNode = ReactDOM.findDOMNode(this);
+		var elementNode = this.elementNode = ReactDOM.findDOMNode(this);
 
 		//instantiate and save reference to the Kendo UI widget on elementNode
+		//note I am not using jQuery plugin to instantiate, don't want to wait for namespace on $.fn
 		this.widgetInstance = new kuidropdown.ui.DropDownList(elementNode,this.props.options);
 
-		//if props are avaliable for events, triggers, unbind events, or methods make it happen
+		//if props are avaliable for events, triggers, unbind events, or methods make it happen now
 		this.props.events ? this.bindEventsToKendoWidget(this.props.events) : null;
 		this.props.methods ? this.callKendoWidgetMethods(this.props.methods) : null;
 		this.props.triggerEvents ? this.triggerKendoWidgetEvents(this.props.triggerEvents) : null;
@@ -25,22 +26,22 @@ const KendoDropDownList = React.createClass({
 
 	//instance methods for updating widget
 	triggerKendoWidgetEvents:function(events){
-		events.forEach(function(event){
+		events.forEach(function(event){//loop over events, and trigger
 			this.widgetInstance.trigger(event);
 		}, this);
 	},
 	bindEventsToKendoWidget:function(events){
-		Object.keys(events).forEach(function(event){
+		Object.keys(events).forEach(function(event){//loop over events and bind
 			this.widgetInstance.bind(event,events[event]);
 		}, this);
 	},
 	unbindEventsToKendoWidget:function(events){
-		events.forEach(function(event){
+		events.forEach(function(event){//loop ove revents and unbind
 			this.widgetInstance.unbind(event);
 		}, this);
 	},
 	callKendoWidgetMethods:function(methods){
-		Object.keys(methods).forEach(function(method){
+		Object.keys(methods).forEach(function(method){//loop over methods and call
 		    this.widgetInstance[method](...methods[method])
 		}, this);
 	},
@@ -78,15 +79,15 @@ const KendoDropDownList = React.createClass({
 		}
   	},
 
-	//Don't run render again, create widget once, then leave it alone
+	//don't run render again, create widget once, then leave it alone
 	shouldComponentUpdate: function(){return false;},
 
-	//Destory it, when the component is unmouted
+	//destory it, when the component is unmouted
 	componentWillUnmount: function() {
-		$(ReactDOM.findDOMNode(this)).getKendoDropDownList().destroy();
+		$(this.elementNode).getKendoDropDownList().destroy();
 	},
 
-	//Use the passed in React nodes or a plain <div> if no react nodes child nodes are defined
+	//use the passed in React nodes or a plain <div> if no React child nodes are defined
 	render: function() {
 		return this.props.children ? this.props.children : <div/>;
 	}
