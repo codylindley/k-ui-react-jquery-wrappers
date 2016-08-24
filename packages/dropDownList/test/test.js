d6cb4efd125810366fa63e5bb482fe8da94f6b45
Cody Lindley <contact@codylindley.com>
2016-08-23 20:52:08 -0600

    updates

--- packages/dropDownList/test/test.js
+++ packages/dropDownList/test/test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow,mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Component from '../src/index.js';
 
 describe("A suite", function() {
 	it("minimal test for component", function() {
-  	const wrapper = mount(<Component />);
-      expect(wrapper.unrendered.type.displayName).to.equal('KendoCalendar');
+  	const wrapper = shallow(<Component />);
+      expect(wrapper.unrendered.type.displayName).to.equal('KendoDropDownList');
     });
 });
--- packages/listView/src/index.js
+++ packages/listView/src/index.js
@@ -19,7 +19,7 @@ const KendoListView = React.createClass({
 
 		//instantiate and save reference to the Kendo UI widget on elementNode
 		//note I am not using jQuery plugin to instantiate, don't want to wait for namespace on $.fn
-		this.widgetInstance = new kuiListView.ui.Calendar(elementNode,this.props.options);
+		this.widgetInstance = new kuiListView.ui.ListView(elementNode,this.props.options);
 
 		//if props are avaliable for events, triggers, unbind events, or methods make it happen now
 		this.props.events ? this.bindEventsToKendoWidget(this.props.events) : null;
